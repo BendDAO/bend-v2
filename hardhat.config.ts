5a7538e78708e94245d7b119d33673c2eee31bc1
Thor Seldon <thorseldon@protonmail.com>
2023-11-07 11:10:12 +0800

    integrating with foundry in hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,36 @@
+import * as dotenv from 'dotenv';
+import fs from 'fs';
+
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-preprocessor';
+
+// Support for foundry remappings: https://book.getfoundry.sh/config/hardhat
+const remappings = fs
+  .readFileSync('remappings.txt', 'utf8')
+  .split('\n')
+  .filter(Boolean)
+  .map((line) => line.trim().split('='));
+
+dotenv.config({ path: './.env.local' });
 
 const config: HardhatUserConfig = {
   solidity: '0.8.19',
+  preprocess: {
+    eachLine: () => ({
+      transform: (line) => {
+        if (line.match(/^\s*import /i)) remappings.forEach(([find, replace]) => (line = line.replace(find, replace)));
+        return line;
+      },
+    }),
+  },
+  paths: {
+    sources: './src',
+    tests: './test-ts',
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
