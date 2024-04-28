86dc2cb7330a7eb798b59e6049e53480e139a86f
Thor Seldon <thorseldon@protonmail.com>
2024-04-28 21:24:44 +0800

    add yield abi files

--- script/extractABI.sh
+++ script/extractABI.sh
@@ -2,12 +2,14 @@
 # NOTE: THis script requires the jq utlity or be installed
 # for each contract in source folder, extract abi from artifact and create new json
 
+# if directory doesn't exist, then create it
+rm -rf release/abis
+mkdir -p release/abis
+
 for contractPath in src/*.sol
 do
     fileWithExtension=${contractPath##*/}
     filename=${fileWithExtension%.*}
-    # if directory doesn't exist, then create it
-    mkdir -p release/abis
     jq '.abi' out/${fileWithExtension}/${filename}.json > release/abis/${filename}.json
 done
 
@@ -15,7 +17,10 @@ for contractPath in src/modules/*.sol
 do
     fileWithExtension=${contractPath##*/}
     filename=${fileWithExtension%.*}
-    # if directory doesn't exist, then create it
-    mkdir -p release/abis
     jq '.abi' out/${fileWithExtension}/${filename}.json > release/abis/${filename}.json
 done
+
+jq '.abi' out/YieldRegistry.sol/YieldRegistry.json > release/abis/YieldRegistry.json
+jq '.abi' out/YieldAccount.sol/YieldAccount.json > release/abis/YieldAccount.json
+jq '.abi' out/YieldEthStakingLido.sol/YieldEthStakingLido.json > release/abis/YieldEthStakingLido.json
+jq '.abi' out/YieldEthStakingEtherfi.sol/YieldEthStakingEtherfi.json > release/abis/YieldEthStakingEtherfi.json
